86ebb4311ed148695a66d56599d920553dd2c31c
mdzaky93 <mdzaky93@gmail.com>
2020-08-04 09:20:17 +0700

    push2

---
+++ src/06.js
@@ -0,0 +1,6 @@
+
+function repeatString(text, times = 1) {
+	return text.repeat(times);
+}
+
+module.exports = repeatString;
--- test/02.test.js
+++ test/02.test.js
@@ -1,7 +1,7 @@
 const sum = require('../src/02.js');
 
-const x = 7;
-const y = 11;
+const x = 11;
+const y = 7;
 test(`Penjumlahan x dan y, x = ${x}, y = ${y}, result = ${x+y}`, () => {
   expect(sum(3,5)).toBe((3+5));
 });
